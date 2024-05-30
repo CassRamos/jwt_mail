cec820370f92e3f7c46a01231f762f53f7a8631c
Cass <cassianoleaoramos@gmail.com>
2024-05-30 17:33:53 -0300

    feat: add user, token and roles layer

--- src/main/java/cass/jwt_mail/JwtMailApplication.java
+++ src/main/java/cass/jwt_mail/JwtMailApplication.java
@@ -2,7 +2,9 @@ package cass.jwt_mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class JwtMailApplication {
 
---
+++ src/main/java/cass/jwt_mail/role/RoleRepository.java
@@ -0,0 +1,6 @@
+package cass.jwt_mail.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
